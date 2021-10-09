60556d4cb8a835bf527c794ae2ca35bfd9b9b1ab
Ihor Vodianyi <vodyanoy_igor@hotmail.com>
2021-10-09 15:07:11 +0300

    Commit

--- src/uuids.adb
+++ src/uuids.adb
@@ -1,5 +1,22 @@
 package body UUIDs is
 
+   function To_String(Self : UUID) return String is
+   begin
+      return "{0}";
+   end To_String;
    
+   function From_String(Value : String) return UUID is
+   begin
+      return Create_New;
+   end From_String;
+   
+    
+   
+   function Create_New return UUID is
+      Res : UUID;
+   begin
+      return Res;
+   end;
+       
 
 end UUIDs;
--- src/uuids.ads
+++ src/uuids.ads
@@ -1,5 +1,21 @@
+with Interfaces; use Interfaces;
+
 package UUIDs is
 
+   type UUID is private;
    
+   function Create_New return UUID;
+   function To_String(Self : UUID) return String;
+   function From_String(Value : String) return UUID;
+   
+private
+   
+   UUID_Size : constant Integer := 16;
+   
+   type ByteArray is array (0 .. UUID_Size - 1) of Unsigned_8;
+   
+   type UUID is record
+      Data : ByteArray := (others => 0);
+   end record;
 
 end UUIDs;
